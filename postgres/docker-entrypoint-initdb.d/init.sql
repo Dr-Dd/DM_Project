211ff31c845f6040b5482002f78017df3f79f6a0
Dr-Dd <idone.leonardo@gmail.com>
2026-02-12 22:59:51 +0100

    Working version. Before blocks.

--- postgres/docker-entrypoint-initdb.d/init.sql
+++ postgres/docker-entrypoint-initdb.d/init.sql
@@ -1,13 +1,13 @@
 -- title.basics.tsv.gz OK
-CREATE TABLE titleType (
+CREATE UNLOGGED TABLE titleType (
   titleTypeId serial PRIMARY KEY,
   titleTypeName text UNIQUE NOT NULL
 );
-CREATE TABLE genre (
+CREATE UNLOGGED TABLE genre (
   genreId serial PRIMARY KEY,
   genreName text UNIQUE NOT NULL
 );
-CREATE TABLE titleBasics (
+CREATE UNLOGGED TABLE titleBasics (
   tconst text PRIMARY KEY,
   primaryTitle text NOT NULL,
   originalTitle text NOT NULL,
@@ -15,33 +15,33 @@ CREATE TABLE titleBasics (
   startYear date,
   endYear date,
   runtimeMinutes int,
-  titleTypeId serial REFERENCES titleType(titleTypeId)
+  titleTypeId integer REFERENCES titleType(titleTypeId)
 );
-CREATE TABLE titleBasics_genre (
+CREATE UNLOGGED TABLE titleBasics_genre (
   tconst text REFERENCES titleBasics(tconst),
-  genreId serial REFERENCES genre(genreId),
+  genreId integer REFERENCES genre(genreId),
   PRIMARY KEY(tconst, genreId)
 );
 
 
 -- name.basics.tsv.gz
-CREATE TABLE nameBasics (
+CREATE UNLOGGED TABLE nameBasics (
   nconst text PRIMARY KEY,
   primaryName text NOT NULL,
   birthYear date NOT NULL,
   deathYear date
 );
-CREATE TABLE profession (
+CREATE UNLOGGED TABLE profession (
   professionId serial PRIMARY KEY,
   professionName text UNIQUE NOT NULL
 );
-CREATE TABLE profession_nameBasics (
-  professionId serial REFERENCES profession(professionId),
+CREATE UNLOGGED TABLE profession_nameBasics (
+  professionId integer REFERENCES profession(professionId),
   nconst text REFERENCES nameBasics(nconst),
   PRIMARY KEY(professionId, nconst)
 );
 -- Just a junction table
-CREATE TABLE knownForTitles (
+CREATE UNLOGGED TABLE knownForTitles (
   tconst text REFERENCES titleBasics(tconst),
   nconst text REFERENCES nameBasics(nconst),
   PRIMARY KEY (nconst, tconst)
@@ -49,50 +49,50 @@ CREATE TABLE knownForTitles (
 
 
 -- title.akas.tsv.gz OK
-CREATE TABLE language (
+CREATE UNLOGGED TABLE language (
   languageId serial PRIMARY KEY,
   languageName text UNIQUE NOT NULL
 );
-CREATE TABLE region (
+CREATE UNLOGGED TABLE region (
   regionId serial PRIMARY KEY,
   regionName text UNIQUE NOT NULL
 );
-CREATE TABLE titleAkas (
+CREATE UNLOGGED TABLE titleAkas (
   akasId serial PRIMARY KEY,
   titleId text REFERENCES titleBasics(tconst) NOT NULL,
   ordering smallint NOT NULL,
   title text NOT NULL,
-  regionId serial REFERENCES region(regionId),
-  languageId serial REFERENCES language(languageId),
+  regionId integer REFERENCES region(regionId),
+  languageId integer REFERENCES language(languageId),
   isOriginalTitle boolean NOT NULL
 );
-CREATE TABLE akaType (
+CREATE UNLOGGED TABLE akaType (
   akaTypeId serial PRIMARY KEY,
   akaTypeName text UNIQUE NOT NULL
 );
-CREATE TABLE akaType_titleAkas (
-  akaTypeId serial REFERENCES akaType(akaTypeId),
-  titleId serial REFERENCES titleAkas(akasId),
+CREATE UNLOGGED TABLE akaType_titleAkas (
+  akaTypeId integer REFERENCES akaType(akaTypeId),
+  titleId integer REFERENCES titleAkas(akasId),
   PRIMARY KEY(akaTypeId, titleId)
 );
-CREATE TABLE attribute (
+CREATE UNLOGGED TABLE attribute (
   attributeId serial PRIMARY KEY,
   attributeText text UNIQUE NOT NULL
 );
-CREATE TABLE attribute_titleAkas (
-  attributeId serial REFERENCES attribute(attributeId),
-  akasId serial REFERENCES titleAkas(akasId),
+CREATE UNLOGGED TABLE attribute_titleAkas (
+  attributeId integer REFERENCES attribute(attributeId),
+  akasId integer REFERENCES titleAkas(akasId),
   PRIMARY KEY(attributeId, akasId)
 );
 
 
 -- title.crew.tsv.gz
-CREATE TABLE director (
+CREATE UNLOGGED TABLE director (
   tconst text REFERENCES titleBasics(tconst),
   nconst text REFERENCES nameBasics(nconst),
   PRIMARY KEY (tconst, nconst)
 );
-CREATE TABLE writer (
+CREATE UNLOGGED TABLE writer (
   tconst text REFERENCES titleBasics(tconst),
   nconst text REFERENCES nameBasics(nconst),
   PRIMARY KEY (tconst, nconst)
@@ -100,7 +100,7 @@ CREATE TABLE writer (
 
 
 -- title.episode.tsv.gz
-CREATE TABLE titleEpisode (
+CREATE UNLOGGED TABLE titleEpisode (
   episodeId serial PRIMARY KEY,
   tconst text REFERENCES titleBasics(tconst),
   parentTconst text REFERENCES titleBasics(tconst),
@@ -109,30 +109,30 @@ CREATE TABLE titleEpisode (
 );
 
 -- title.principals.tsv.gz
-CREATE TABLE category (
+CREATE UNLOGGED TABLE category (
   categoryId serial PRIMARY KEY,
   categoryName text UNIQUE
 );
-CREATE TABLE job (
+CREATE UNLOGGED TABLE job (
   jobId serial PRIMARY KEY,
   jobName text UNIQUE
 );
-CREATE TABLE character (
+CREATE UNLOGGED TABLE character (
   characterId serial PRIMARY KEY,
   characterName text UNIQUE
 );
-CREATE TABLE titlePrincipals (
+CREATE UNLOGGED TABLE titlePrincipals (
   tconst text REFERENCES titleBasics(tconst),
   ordering smallint,
   nconst text REFERENCES nameBasics(nconst),
-  categoryId serial REFERENCES category(categoryId),
-  jobId serial REFERENCES job(jobId),
-  characterId serial REFERENCES character(characterId)
+  categoryId integer REFERENCES category(categoryId),
+  jobId integer REFERENCES job(jobId),
+  characterId integer REFERENCES character(characterId)
 );
 
 
 -- title.ratings.tsv.gz
-CREATE TABLE ratings (
+CREATE UNLOGGED TABLE ratings (
   tconst text REFERENCES titleBasics(tconst),
   averageRating decimal(3,1),
   check (averageRating >= 0 AND averageRating <= 10),
--- python/etl.py
+++ python/etl.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python3
 # Data configuration file
-import time
 import os
 import gzip
 import psycopg
 import csv
 import re
+import threading
+
+BLOCK_SIZE = 4 * 1024 * 1024 * 1024
 
 data_files = [
     "name.basics.tsv.gz",
-    "title.akas.tsv.gz",
     "title.basics.tsv.gz",
+    "title.akas.tsv.gz",
     "title.crew.tsv.gz",
     "title.episode.tsv.gz",
     "title.principals.tsv.gz",
@@ -212,32 +214,31 @@ def dateify(y):
         return None
     return f"{int(y):04d}-01-01"
 
-def load_data():
-    with psycopg.connect(conn_string) as conn:
-        with conn.cursor() as cur:
-            handle_files(cur)
-        conn.commit()
-
-def get_id_by_value(data, target_v):
-    for id_, v in data:
-        if v == target_v:
-            return id_
-    return None
-
 def type_split(word):
     return type_pattern.findall(word)
 
 def attribute_split(word):
     return attribute_pattern.findall(word)
 
-def handle_files(cur):
+def handle_files():
     for f in data_files:
+        print(f"Processing {f}", flush=True)
         with gzip.open(os.path.join("/data", f), "rt") as d:
-            r = csv.DictReader(d, delimiter="\t", quoting=csv.QUOTE_NONE)
             match f:
                 case "title.basics.tsv.gz":
-                    with (cur.copy("COPY titleType (titleTypeId, titleTypeName) FROM STDIN") as cp_titleType,
-                          cur.copy("""COPY titleBasics (
+                    r = csv.DictReader(d, delimiter="\t", quoting=csv.QUOTE_NONE)
+                    conn_1 = psycopg.connect(conn_string)
+                    conn_2 = psycopg.connect(conn_string)
+                    conn_3 = psycopg.connect(conn_string)
+                    conn_4 = psycopg.connect(conn_string)
+                    cur_1 = conn_1.cursor()
+                    cur_2 = conn_2.cursor()
+                    cur_3 = conn_3.cursor()
+                    cur_4 = conn_4.cursor()
+                    cur_2.execute("ALTER TABLE titleBasics DISABLE TRIGGER ALL;")
+                    cur_4.execute("ALTER TABLE titleBasics_genre DISABLE TRIGGER ALL;")
+                    with (cur_1.copy("COPY titleType (titleTypeId, titleTypeName) FROM STDIN") as cp_titleType,
+                          cur_2.copy("""COPY titleBasics (
                           tconst,
                           primaryTitle,
                           originalTitle,
@@ -247,31 +248,22 @@ def handle_files(cur):
                           runtimeMinutes,
                           titleTypeId) FROM STDIN
                           """) as cp_titleBasics,
-                          cur.copy("COPY genre (genreId, genreName) FROM STDIN") as cp_genre,
-                          cur.copy("COPY titleBasics_genre (tconst, genreId) FROM STDIN") as cp_titleBasics_genre
+                          cur_3.copy("COPY genre (genreId, genreName) FROM STDIN") as cp_genre,
+                          cur_4.copy("COPY titleBasics_genre (tconst, genreId) FROM STDIN") as cp_titleBasics_genre
                           ):
+                        print("Connected for titles.", flush=True)
                         titleType_rows = {}
                         titleTypeId = 1
                         genre_rows = {}
                         genreId = 1
                         for l in r:
-                            if l["titleType"] != "\\N" and l["titleType"] not in titleType_rows:
-                                titleType_rows[l["titleType"]] = titleTypeId
-                                titleTypeId += 1
-                                cp_titleType.write_row((
-                                    titleType_rows[l["titleType"]], l["titleType"]
-                                ))
-                            if l["genres"] != "\\N":
-                                for g in l["genres"].split(","):
-                                    if g not in genre_rows:
-                                        genre_rows[l["genre"]] = genreId
-                                        genreId += 1
-                                        cp_genre.write_row((
-                                            genre_rows[l["genre"]], l["genre"]
-                                        ))
-                                        cp_titleBasics_genre.write_row((
-                                            l["tconst"], genre_rows[l["genre"]]
-                                        ))
+                            if l["titleType"] != "\\N":
+                                if l["titleType"] not in titleType_rows:
+                                    titleType_rows[l["titleType"]] = titleTypeId
+                                    titleTypeId += 1
+                                    cp_titleType.write_row((
+                                        titleType_rows[l["titleType"]], l["titleType"]
+                                    ))
                             cp_titleBasics.write_row((
                                 l["tconst"],
                                 l["primaryTitle"],
@@ -282,10 +274,45 @@ def handle_files(cur):
                                 pg_null(l["runtimeMinutes"]),
                                 pg_null(titleType_rows[l["titleType"]])
                             ))
+                            if l["genres"] != "\\N":
+                                for g in l["genres"].split(","):
+                                    if g not in genre_rows:
+                                        genre_rows[g] = genreId
+                                        genreId += 1
+                                        cp_genre.write_row((
+                                            genre_rows[g], g
+                                        ))
+                                    cp_titleBasics_genre.write_row((
+                                        l["tconst"], genre_rows[g]
+                                    ))
+                    conn_1.commit()
+                    conn_2.commit()
+                    conn_3.commit()
+                    conn_4.commit()
+                    cur_2.execute("ALTER TABLE titleBasics ENABLE TRIGGER ALL;")
+                    cur_4.execute("ALTER TABLE titleBasics_genre ENABLE TRIGGER ALL;")
                 case "title.akas.tsv.gz":
-                    with(cur.copy("COPY language (languageId, languageName) FROM STDIN") as cp_language,
-                         cur.copy("COPY region (regionId, regionName)") as cp_region,
-                         cur.copy("""
+                    r = csv.DictReader(d, delimiter="\t", quoting=csv.QUOTE_NONE)
+                    conn_1 = psycopg.connect(conn_string)
+                    conn_2 = psycopg.connect(conn_string)
+                    conn_3 = psycopg.connect(conn_string)
+                    conn_4 = psycopg.connect(conn_string)
+                    conn_5 = psycopg.connect(conn_string)
+                    conn_6 = psycopg.connect(conn_string)
+                    conn_7 = psycopg.connect(conn_string)
+                    cur_1 = conn_1.cursor()
+                    cur_2 = conn_2.cursor()
+                    cur_3 = conn_3.cursor()
+                    cur_4 = conn_4.cursor()
+                    cur_5 = conn_5.cursor()
+                    cur_6 = conn_6.cursor()
+                    cur_7 = conn_7.cursor()
+                    cur_3.execute("ALTER TABLE titleAkas DISABLE TRIGGER ALL;")
+                    cur_5.execute("ALTER TABLE akaType_titleAkas DISABLE TRIGGER ALL;")
+                    cur_7.execute("ALTER TABLE attribute_titleAkas DISABLE TRIGGER ALL;")
+                    with(cur_1.copy("COPY language (languageId, languageName) FROM STDIN") as cp_language,
+                         cur_2.copy("COPY region (regionId, regionName) FROM STDIN") as cp_region,
+                         cur_3.copy("""
                          COPY titleAkas (
                          akasId,
                          titleId,
@@ -294,13 +321,14 @@ def handle_files(cur):
                          regionId,
                          languageId,
                          isOriginalTitle
-                         )
+                         ) FROM STDIN
                          """) as cp_titleAkas,
-                         cur.copy("COPY akaType (akaTypeId, akaTypeName)") as cp_akaType,
-                         cur.copy("COPY akaType_titleAkas (akaTypeId, titleId)") as cp_akaType_titleAkas,
-                         cur.copy("COPY attribute (attributeId, attributeText)") as cp_attribute,
-                         cur.copy("COPY attribute_titleAkas (attributeId, akasId)") as cp_attribute_titleAkas
+                        cur_4.copy("COPY akaType (akaTypeId, akaTypeName) FROM STDIN") as cp_akaType,
+                        cur_5.copy("COPY akaType_titleAkas (akaTypeId, titleId) FROM STDIN") as cp_akaType_titleAkas,
+                        cur_6.copy("COPY attribute (attributeId, attributeText) FROM STDIN") as cp_attribute,
+                        cur_7.copy("COPY attribute_titleAkas (attributeId, akasId) FROM STDIN") as cp_attribute_titleAkas
                          ):
+                        print("Connected for akas.")
                         language_id_map = {}
                         language_id = 1
                         region_id_map = {}
@@ -311,34 +339,36 @@ def handle_files(cur):
                         attribute_id = 1
                         titleAkas_id = 1
                         for l in r:
-                            if l["language"] != "\\N"and l["language"] not in language_id_map:
-                                language_id_map[l["language"]] = language_id
-                                language_id += 1
-                                cp_language.write_row((
-                                    language_id_map[l["language"]], l["language"]
-                                ))
-                            if l["region"] != "\\N" and l["region"] not in region_id_map:
-                                region_id_map[l["region"]] = region_id
-                                region_id += 1
-                                cp_region.write_row((
-                                    region_id_map[l["region"]], l["region"]
-                                ))
+                            if l["language"] != "\\N":
+                                if l["language"] not in language_id_map:
+                                    language_id_map[l["language"]] = language_id
+                                    language_id += 1
+                                    cp_language.write_row((
+                                        language_id_map[l["language"]], l["language"]
+                                    ))
+                            if l["region"] != "\\N":
+                                if l["region"] not in region_id_map:
+                                    region_id_map[l["region"]] = region_id
+                                    region_id += 1
+                                    cp_region.write_row((
+                                        region_id_map[l["region"]], l["region"]
+                                    ))
                             if l["types"] != "\\N":
                                 for t in type_split(l["types"]):
                                     if t not in akaType_id_map:
                                         akaType_id_map[t] = akaType_id
                                         akaType_id += 1
                                         cp_akaType.write_row((akaType_id_map[t], t))
-                                        cp_akaType_titleAkas.write_row((
-                                            akaType_id_map[t], l["titleId"]
-                                        ))
+                                    cp_akaType_titleAkas.write_row((
+                                        akaType_id_map[t], titleAkas_id
+                                    ))
                             cp_titleAkas.write_row((
                                 titleAkas_id,
                                  l["titleId"],
                                  l["ordering"],
                                  l["title"],
-                                 pg_null(region_id_map[l["region"]]),
-                                 pg_null(language_id_map[l["language"]]),
+                                 pg_null(region_id_map.get(l["region"])),
+                                 pg_null(language_id_map.get(l["language"])),
                                  l["isOriginalTitle"]
                             ))
                             if l["attributes"] != "\\N":
@@ -347,16 +377,26 @@ def handle_files(cur):
                                         attribute_id_map[t] = attribute_id
                                         attribute_id += 1
                                         cp_attribute.write_row((attribute_id_map[t], t))
-                                        cp_attribute_titleAkas.write_row((
-                                            attribute_id_map[t],
-                                            titleAkas_id
-                                        ))
+                                    cp_attribute_titleAkas.write_row((
+                                        attribute_id_map[t],
+                                        titleAkas_id
+                                    ))
                             titleAkas_id += 1
+                    conn_1.commit()
+                    conn_2.commit()
+                    conn_3.commit()
+                    conn_4.commit()
+                    conn_5.commit()
+                    conn_6.commit()
+                    conn_7.commit()
+                    cur_3.execute("ALTER TABLE titleAkas ENABLE TRIGGER ALL;")
+                    cur_5.execute("ALTER TABLE akaType_titleAkas ENABLE TRIGGER ALL;")
+                    cur_7.execute("ALTER TABLE attribute_titleAkas ENABLE TRIGGER ALL;")
 
 
 
 
 print("Starting etl process...", flush=True)
-load_data()
+handle_files()
 print("Etl process completed, exiting.", flush=True)
 exit(0)
